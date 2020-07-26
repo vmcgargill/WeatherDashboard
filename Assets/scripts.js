0932e5cfdd1261e61790ff375ef77c523d8c800e
Vincent McGargill <vincentmcgargill@gmail.com>
2020-07-25 23:15:17 -0600

    Added 5 day forecast, UV index, and geolocation waether

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,26 +1,88 @@
 const APIKey = config.SECRET_KEY;
-const OpwenWeatherLink = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const OpwenWeatherLink2 = '&appid=';
-const DefaultLocation = 'Denver';
 
+function GetTodaysWeather(weatherURL) {
+  var CurrentDate = moment().format('dddd, MMMM Do, YYYY');
 
-
-function GetCityWeatherData(location) {
-    var queryURL = OpwenWeatherLink + location + OpwenWeatherLink2 + APIKey
-
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).then(function(response) {
-      $('#CurrentCity').text(response.name);
-      $('#CurrentCityTemp').text('Tempature: ' + Math.floor(response.main.temp * (9/5) - 459.67) + ' °F');
-      $('#CurrentCityHum').text('Humidity: ' + response.main.humidity + '%');
-      $('#CurrentCityWS').text('Wind Speed: ' + response.wind.speed + ' MPH');
-      $('#CurrentCityUV').text('UV Index: ' + '9.49');
-    });
+  $.ajax({
+    url: weatherURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response)
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    GetUVIndex(lat, lon);
+    $('#CurrentCity').text(response.name + " - " + CurrentDate);
+    $('#CurrentCityTemp').text('Tempature: ' + Math.floor(response.main.temp * (9/5) - 459.67) + ' °F');
+    $('#CurrentCityHum').text('Humidity: ' + response.main.humidity + '%');
+    $('#CurrentCityWS').text('Wind Speed: ' + response.wind.speed + ' MPH');
+  });
 }
 
-GetCityWeatherData(DefaultLocation);
+function GetUVIndex(lat, lon) {
+  var UVIndexURL = 'http://api.openweathermap.org/data/2.5/uvi?appid=' + APIKey + '&lat=' + lat + '&lon=' + lon
+  $.ajax({
+    url: UVIndexURL,
+    method: 'GET'
+  }).then(function(response) {
+    const CurrentCityUV = $('#CurrentCityUV');
+    if (response.value <= 2) {
+      CurrentCityUV.html("UV Index: " + '<a class="greenUV">' + response.value + '</a>');
+    } else if (response.value > 2 && response.value < 5) {
+      CurrentCityUV.html("UV Index: " + '<a class="yellowUV">' + response.value + '</a>');
+    } else if (response.value >= 5 && response.value < 7) {
+      CurrentCityUV.html("UV Index: " + '<a class="orangeUV">' + response.value + '</a>');
+    } else if (response.value >= 7 && response.value < 10) {
+      CurrentCityUV.html("UV Index: " + '<a class="redUV">' + response.value + '</a>');
+    } else if (response.value >= 10) {
+      CurrentCityUV.html("UV Index: " + '<a class="purpleUV">' + response.value + '</a>');
+    }
+  });
+}
+
+function GetWeatherForecast(forecastURL) {
+  $.ajax({
+    url: forecastURL,
+    method: 'GET'
+  }).then(function(response) {
+    $('#FutureForcastDiv').text("");
+    for (i = 3; i < 36; i+=8) {
+      var day = $('<div>').attr('class', 'FutureForcast');
+      $('#FutureForcastDiv').append(day);
+      var Date = new moment(response.list[i].dt_txt);
+      $(day).append($('<p>').text(Date.format('dddd, MM/DD/YYYY') + " @12pm"));
+      var WeatherIconURL = 'http://openweathermap.org/img/wn/' + response.list[i].weather[0].icon + '@2x.png';
+      var IMG1 = $('<img>').attr("src", WeatherIconURL);
+      IMG1.width(40);
+      $(day).append(IMG1);
+      $(day).append($('<p>').text('Temp: ' + Math.floor(response.list[i].main.temp * (9/5) - 459.67) + ' °F'));
+      $(day).append($('<p>').text('Humidity: ' + response.list[i].main.humidity + '%'));
+    }
+  });
+}
+
+function GetCityWeatherData(location) {
+  var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIKey
+  GetTodaysWeather(weatherURL);
+
+  var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + location + '&appid=' + APIKey
+  GetWeatherForecast(forecastURL);
+}
+
+function GetCordWeatherData() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showLocationWeather);
+  }
+  
+  function showLocationWeather(position) {
+    var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + 
+    position.coords.latitude + '&lon=' + position.coords.longitude +'&appid=' + APIKey
+    GetTodaysWeather(weatherURL);
+
+    var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + 
+    position.coords.latitude + '&lon=' + position.coords.longitude +'&appid=' + APIKey
+    GetWeatherForecast(forecastURL);
+  }
+}
 
 $(document).on('click','.city',function(){
   var CityValue = $(this).val();
@@ -31,3 +93,6 @@ $(document).on('click','#SearchLocation',function(){
   var LocationValue = $('#SearchLocationInput').val();
   GetCityWeatherData(LocationValue);
 });
+
+/// Default Location ///
+GetCityWeatherData('Denver');
